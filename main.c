6866bea0a72bb1c42ce4bea7b03f916c22e624ce
Lucas Jorge <lucas_jorg@hotmail.com>
2022-12-26 15:56:05 -0300

    study notes #1

--- main.c
+++ main.c
@@ -3,40 +3,39 @@
 #include <unistd.h>
 #include <pthread.h>
 
-char l[5] = "false";
+void* myTurn(void *arg){
 
-void *myThreadFun2(void *vargp){
-
-    strcpy(l, "true");
-
-    printf("Second Thread, l = %s\n", l);
+    while(1){
+        sleep(1);
+        printf("My Turn!\n");
+    }
 
     return NULL;
 
 }
 
-void *myThreadFun(void *vargp){
+void yourTurn(){
 
-    printf("First Thread, l = %s\n", l);
-
-    pthread_t thread_id_2;
-    pthread_create(&thread_id_2, NULL, myThreadFun2, NULL);
-    pthread_join(thread_id_2, NULL);
-
-    return NULL;
+    while(1){
+        sleep(1);
+        printf("Your Turn!\n");
+    }
 }
 
 int main(){
 
     printf("Starting ...\n");
+
     sleep(3);
-    pthread_t thread_id;
+
     printf("Before Thread\n");
-    pthread_create(&thread_id, NULL, myThreadFun, NULL);
-    pthread_join(thread_id, NULL);
-    printf("After Thread\n");
-    printf("l = %s\n", l);
-    printf("... Done");
+
+    pthread_t thread_id;
+
+    pthread_create(&thread_id, NULL, myTurn, NULL);
+
+    yourTurn();
+
     exit(0);
 
 }
